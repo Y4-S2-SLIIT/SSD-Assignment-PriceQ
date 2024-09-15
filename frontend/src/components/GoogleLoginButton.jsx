293e91602a9812e277ff5e5603e6bf85566842b3
Randula Mawaththa <randula98@gmail.com>
2024-09-15 22:36:54 +0530

    feat: Update OAuth env
    Signed-off-by: Randula Mawaththa <randula98@gmail.com>

--- frontend/src/components/GoogleLoginButton.jsx
+++ frontend/src/components/GoogleLoginButton.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 import Swal from "sweetalert2";
-
 import UserService from "../services/user.service";
+import Button from "react-bootstrap/Button"; // Import Bootstrap Button for consistent styling
 
 export default function GoogleLoginButton() {
+    const [isLoading, setIsLoading] = useState(false); // Loading state
 
     const responseMessage = (response) => {
+        setIsLoading(true); // Set loading to true
         const decodedToken = jwtDecode(response.credential);
 
         UserService.getUserByEmail(decodedToken.email)
@@ -19,7 +21,6 @@ export default function GoogleLoginButton() {
                 }
             })
             .then((data) => {
-                // Access the user data
                 if (data.status) {
                     Swal.fire({
                         icon: "success",
@@ -33,21 +34,20 @@ export default function GoogleLoginButton() {
                     setTimeout(() => {
                         window.location.href = "/dashboard";
                     }, 1500);
-                }
-                else {
+                } else {
                     const data = {
                         firstName: decodedToken.given_name,
                         lastName: decodedToken.family_name,
                         email: decodedToken.email,
                         image: decodedToken.picture,
                         isoAuth: true,
-                    }
+                    };
                     UserService.register(data)
                         .then((response) => {
                             if (response.ok) {
                                 return response.json(); // Parse the JSON body
                             } else {
-                                throw new Error('Failed to fetch user data');
+                                throw new Error('Failed to register user');
                             }
                         })
                         .then((data) => {
@@ -65,28 +65,56 @@ export default function GoogleLoginButton() {
                         .catch((error) => {
                             console.error('Error:', error);
                         });
-
                 }
             })
             .catch((error) => {
                 console.error('Error:', error);
+                Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: "Error occurred during login!",
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            })
+            .finally(() => {
+                setIsLoading(false); // Set loading to false after the process
             });
-    }
-
+    };
 
     const errorMessage = (error) => {
         console.log(error);
+        setIsLoading(false); // Set loading to false if error occurs
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Google Login Failed!",
+            showConfirmButton: false,
+            timer: 1500,
+        });
     };
-
 
     return (
         <div id="signInButton">
-            <GoogleLogin
-                onSuccess={responseMessage}
-                onError={errorMessage}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={true}
-            />
+            {isLoading ? (
+                <Button variant="primary" disabled>
+                    <span
+                        className="spinner-border spinner-border-sm"
+                        role="status"
+                        aria-hidden="true"
+                    ></span>
+                    &nbsp;
+                    &nbsp;
+                    Loading...
+                </Button>
+            ) : (
+                <GoogleLogin
+                    onSuccess={responseMessage}
+                    onError={errorMessage}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+            )}
         </div>
-    )
+    );
 }

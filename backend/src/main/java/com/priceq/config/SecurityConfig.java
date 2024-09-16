3638e8332d9ed28ebd35b1bd513a39a7dee5c12f
Randula Mawaththa <randula98@gmail.com>
2024-09-16 16:03:33 +0530

    feat: CSRF issue fixed
    Signed-off-by: Randula Mawaththa <randula98@gmail.com>

--- backend/src/main/java/com/priceq/config/SecurityConfig.java
+++ backend/src/main/java/com/priceq/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.priceq.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +12,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()  // Disable CSRF protection for your case
                 .headers(headers -> headers
-                        .frameOptions(frameOptions -> frameOptions.deny())
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
                         .httpStrictTransportSecurity(hsts -> hsts  // Enable HSTS
                                 .includeSubDomains(true)
                                 .maxAgeInSeconds(31536000)
@@ -20,7 +22,7 @@ public class SecurityConfig {
                         )
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()  // Ensure all requests are authenticated
+                        .anyRequest().permitAll()  // Ensure all requests are permitted
                 );
 
         return http.build();
